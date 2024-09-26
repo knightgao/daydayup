eec7dae950b17d46720bf13a3e5f887ac0b6c438
knight.gao <gaoye@dreame.tech>
2024-09-26 13:50:09 +0800

    feat: 新增滑动窗口

--- 209.长度最小的子数组.js
+++ 209.长度最小的子数组.js
@@ -11,24 +11,21 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    const len = nums.length;
-    if (len === 0) return 0;
-
+    let len = nums.length;
+    if(len === 0){
+        return 0;
+    }
+    let start = 0;
     let minLen = Infinity;
     let sum = 0;
-    let start = 0;
-    
-    for (let end = 0; end < len; end++) {
+    for(let end = 0; end < len;end++){
         sum += nums[end];
-        
-        while (sum >= target) {
-            minLen = Math.min(minLen, end - start + 1);
-            // 太妙了这部
+        while(sum >= target){
+            minLen = Math.min(minLen,(end-start+1));
             sum -= nums[start];
             start++;
         }
     }
-    
     return minLen === Infinity ? 0 : minLen;
 };
 
