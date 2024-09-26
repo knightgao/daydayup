8df230fb0cf6630f0c9394d8a61949a089d2f9ac
knight.gao <gaoye@dreame.tech>
2024-09-26 11:16:54 +0800

    feat: 新增滑动窗口

---
+++ 209.长度最小的子数组.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=javascript
+ *
+ * [209] 长度最小的子数组
+ */
+
+// @lc code=start
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    const len = nums.length;
+    if (len === 0) return 0;
+
+    let minLen = Infinity;
+    let sum = 0;
+    let start = 0;
+    
+    for (let end = 0; end < len; end++) {
+        sum += nums[end];
+        
+        while (sum >= target) {
+            minLen = Math.min(minLen, end - start + 1);
+            // 太妙了这部
+            sum -= nums[start];
+            start++;
+        }
+    }
+    
+    return minLen === Infinity ? 0 : minLen;
+};
+
+// @lc code=end
+
